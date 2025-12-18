17e5f5677c04fe05d96130269a7ba5023c3b42e3
DuckTieCorpMember <danyshbek1996@gmail.com>
2025-12-18 15:57:40 +0500

    create react version

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,84 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import DataGrid, {
+  Column,
+  FilterRow,
+  Paging,
+  Scrolling,
+  Pager as GridPager,
+} from 'devextreme-react/data-grid';
+import Pagination from 'devextreme-react/pagination';
+import { DataSource } from 'devextreme/common/data';
+import { generateData } from './data';
+
+const allowedPageSizes = [5, 10, 15];
+const initialPageSize = 10;
+const componentWidth = 800;
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+  const [pageIndex, setPageIndex] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(initialPageSize);
+  const dataSource = useMemo(() => {
+    return new DataSource({
+      store: {
+        type: 'array',
+        data: generateData(100000),
+        key: 'id',  
+      },
+      onLoadingChanged: (isLoading: boolean) => {
+        if(!isLoading) {
+          setItemCount(dataSource.totalCount());
+        }
+      }
+    })
+  }, [])
+  const [itemCount, setItemCount] = useState(0);
+
+  const handlePaginationOptionChanged = useCallback((e: any) => {
+    if (e.name === 'pageIndex') {
+      setPageIndex(e.value);
+    } else if (e.name === 'pageSize') {      
+      setPageIndex(1);
+      setPageSize(e.value);
+    }
+  }, []);
+
+  const handleGridOptionChanged = useCallback((e: { fullName?: string }) => {
+    if (e.fullName?.includes('filterValue')) {
+      setPageIndex(1);
+    }
+  }, []);
+
   return (
     <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+      <Pagination
+        width={componentWidth}
+        allowedPageSizes={allowedPageSizes}
+        pageIndex={pageIndex}
+        itemCount={itemCount}
+        pageSize={pageSize}
+        showNavigationButtons={true}
+        showInfo={true}
+        onOptionChanged={handlePaginationOptionChanged}
+      />
+      <div style={{ maxHeight: 800, marginTop: 16 }}>
+        <DataGrid
+          dataSource={dataSource}
+          showBorders={true}
+          width={componentWidth}
+          onOptionChanged={handleGridOptionChanged}
+        >
+          <FilterRow visible={true} />
+          <Scrolling rowRenderingMode="virtual" />
+          <Paging pageSize={pageSize} pageIndex={pageIndex - 1} />
+          <GridPager visible={false} />
+          <Column dataField="firstName" caption="First Name" />
+          <Column dataField="lastName" caption="Last Name" />
+          <Column dataField="gender" caption="Gender" />
+          <Column dataField="birthDate" caption="Birth Date" dataType="date" />
+        </DataGrid>
+      </div>
     </div>
   );
 }
